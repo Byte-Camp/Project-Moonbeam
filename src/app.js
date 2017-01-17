ccff3e1f931ed4e19c0d152757a8d21da78b5633
brandoshizzle <cathcart.brandon@gmail.com>
2017-01-16 23:04:50 -0700

    Added image-data-uri package, saving a captured image now works

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
-// Here is the starting point for your application code.
-// All stuff below is just to show you how it works. You can delete all of it.
 /* jshint esversion: 6 */
 // Use new ES6 modules syntax for everything.
 import os from 'os'; // native node.js module
 import { remote } from 'electron'; // native electron module
 import jetpack from 'fs-jetpack'; // module loaded from npm
-import { greet } from './hello_world/hello_world'; // code authored by you in this project
 import env from './env';
-
-console.log('Loaded environment variables:', env);
 
 var app = remote.app;
 var appDir = jetpack.cwd(app.getAppPath());
 var Webcam = require('./vendor/webcam.min');
+var convertURI = require('image-data-uri');
 
 // Holy crap! This is browser window with HTML and stuff, but I can read
 // here files like it is node.js! Welcome to Electron world :)
@@ -24,5 +20,24 @@ document.addEventListener('DOMContentLoaded', function() {
 		"flip_horiz": true
 	});
 	Webcam.attach('#webcam');
+});
+
+$('#snapButton').click(function(){
+	Webcam.snap( function(data_uri) {
+	// Some image data uri
+	let dataURI = data_uri;
+
+	// It will create the full path in case it doesn't exist
+	// If the extension is defined (e.g. fileName.png), it will be preserved, otherwise the lib will try to guess from the Data URI
+	let filePath = './out/test.png';
+	// Returns a Promise
+	convertURI.outputFile(dataURI, filePath)
+	// RETURNS image path of the created file 'out/path/fileName.png'
+	.then(res => console.log(res));
+		});
+});
+
+$(document).ready(function(){
+	console.log('hi');
 
 });
